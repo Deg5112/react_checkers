141d0c3093f6699acdcf5c07809ccaccf5cf7005
deg5112 <deg5112@gmail.com>
2018-02-16 14:26:43 -0800

    spacing fix

--- src/actions.js
+++ src/actions.js
@@ -1,34 +1,29 @@
 export const setCheckerboard = (checkerBoard) => ({
-	type: 'checkerBoard',
-	value: checkerBoard
+  type: 'checkerBoard',
+  value: checkerBoard
 });
 
 export const setCheckerSelectedToMoveCoordinate = (coordinate) => ({
-	type: 'checkerSelectedToMoveCoordinate',
-	value: coordinate
+  type: 'checkerSelectedToMoveCoordinate',
+  value: coordinate
 });
 
 export const setPossibleMoveCoordinates = (possibleMoveCoordinates) => ({
-	type: 'possibleMoveCoordinates',
-	value: possibleMoveCoordinates
+  type: 'possibleMoveCoordinates',
+  value: possibleMoveCoordinates
 });
 
 export const setCheckerRef = (checkerRef) => ({
-	type: 'checkerRef',
-	value: checkerRef
+  type: 'checkerRef',
+  value: checkerRef
 });
 
 export const removeCheckerRef = (checkerRef) => ({
-	type: 'checkerRefRemove',
-	value: checkerRef
+  type: 'checkerRefRemove',
+  value: checkerRef
 });
 
 export const setCheckerBoardRef = (checkerBoardRef) => ({
-	type: 'checkerBoardRef',
-	value: checkerBoardRef
+  type: 'checkerBoardRef',
+  value: checkerBoardRef
 });
-
-
-
-
-
